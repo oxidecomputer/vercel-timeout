8b6a45afcff60b6a0c110de04905b43328708fcd
David Crespo <david.crespo@oxidecomputer.com>
2023-09-29 15:12:05 -0500

    vary size with query param

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,25 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { RandomBytes } from "~/components/RandomBytes";
 
+// the loader is not strictly necessary -- this repros just fine with 18000
+// hard coded. but letting you specify the number makes it easy to play with the
+// response size
+export function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const size = url.searchParams.get("size");
+  return json({
+    size: size && /^\d+$/.test(size) ? parseInt(size, 10) : 18000,
+  });
+}
+
 export default function Index() {
-  return <RandomBytes n={18000} />;
+  const { size } = useLoaderData<typeof loader>();
+  return (
+    <>
+      size={size} (set with query param)
+      <br />
+      <RandomBytes n={size} />
+    </>
+  );
 }
